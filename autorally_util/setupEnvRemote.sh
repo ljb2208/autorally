8a9321f61c17f60daaf7959b90395948603095aa
Luke Barnett <ljb2208@gmail.com>
2018-04-02 16:39:36 +0100

    More config changes

--- autorally_util/setupEnvRemote.sh
+++ autorally_util/setupEnvRemote.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # source this before you perform a distributed launch with a remote AutoRally chassis
-export ROS_MASTER_URI=http://COMPUTE_BOX_HOSTNAME:11311
-export MASTER_HOSTNAME=COMPUTE_BOX_HOSTNAME
-export MASTER_USER=COMPUTE_BOX_USERNAME
+export ROS_MASTER_URI=http://autorally:11311
+export MASTER_HOSTNAME=autorally
+export MASTER_USER=lbarnett
 export HOSTNAME=$(hostname)
 export ROSLAUNCH_SSH_UNKNOWN=1
 # Find directory of script file to avoid hard-coded paths
