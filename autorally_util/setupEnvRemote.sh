ef1cb724590b9357cbed108284d5d1f58ecb000d
ljb2208 <ljb2208@gmail.com>
2018-11-17 09:56:55 +0000

    update env files

--- autorally_util/setupEnvRemote.sh
+++ autorally_util/setupEnvRemote.sh
@@ -3,7 +3,7 @@
 # source this before you perform a distributed launch with a remote AutoRally chassis
 export ROS_MASTER_URI=http://autorally:11311
 export MASTER_HOSTNAME=autorally
-export MASTER_USER=lbarnett
+export MASTER_USER=ljb2208
 export HOSTNAME=$(hostname)
 export ROSLAUNCH_SSH_UNKNOWN=1
 # Find directory of script file to avoid hard-coded paths
