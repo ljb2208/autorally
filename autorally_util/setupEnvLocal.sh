d6a86d96a88d8456d3cf062afd8c11d31a484486
Luke Barnett <ljb2208@gmail.com>
2020-02-18 19:59:34 -0500

    Changes to launch files to better support sim

--- autorally_util/setupEnvLocal.sh
+++ autorally_util/setupEnvLocal.sh
@@ -5,6 +5,7 @@ export ROS_MASTER_URI=http://localhost:11311
 export MASTER_HOSTNAME=localhost
 export HOSTNAME=localhost
 export ROSLAUNCH_SSH_UNKNOWN=0
+
 # Find directory of script file to avoid hard-coded paths
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/setupEnvVariables.sh
